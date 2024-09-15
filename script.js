a99e49fa8f0c0d33db0a5f7a894cb8c077039cba
Dmytro <dimavaskiv9@gmail.com>
2024-09-15 23:42:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict'
+let pose = 69;
+
+console.log(`https/google::${pose}/idiot`)
+
+let incr = 10;
+let decr = 11;
+
+incr--;
+decr++;
+
+console.log(--incr);
+console.log(++decr);
+
+console.log(incr%3);
+
+console.log(incr === 8);
+
+let a = true;
+let b = true;
+
+console.log( a && b);
